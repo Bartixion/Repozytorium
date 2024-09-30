61e3f2761d4969ea3b97b9520e78538e76f7bdde
Karaś Bartłomiej <karas.bartlomiej@SZKOLA.ZST-RADOM.EDU.PL>
2024-09-30 16:50:40 +0200

    Check Id Feature

---
+++ 2309/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace PeselToAge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int century = 20;
+            int currentYear = DateTime.Now.Year;
+            Console.WriteLine("Podaj swój PESEL:");
+            string pesel = Console.ReadLine();
+
+            while (pesel.Length != 11 || !pesel.All(char.IsDigit))
+            {
+                if (pesel.Length != 11)
+                {
+                    Console.WriteLine("PESEL musi mieć 11 cyfr.");
+                }
+                else if (!pesel.All(char.IsDigit))
+                {
+                    Console.WriteLine("PESEL powinien zawierać tylko cyfry.");
+                }
+                pesel = Console.ReadLine();
+            }
+
+            if (pesel[2] == 2 || pesel[2] == 3)
+            {
+                century = 20;
+            }
+            else if (pesel[2] == 0 || pesel[2] == 1)
+            {
+                century = 19;
+            }
+            string rok = $"{century}{pesel[0]}{pesel[1]}";
+            Console.WriteLine($"urodziles sie w {rok} i masz {currentYear - Convert.ToInt16(rok)}");
+        }
+    }
+}
